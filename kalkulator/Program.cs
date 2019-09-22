8ebb80a57e9322c22a9ed0f956c957a109859301
Piotrek <p.reclaw@gmail.com>
2019-09-22 15:50:52 +0200

    add div to menu Conflict resolved

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -8,13 +8,13 @@ namespace kalkulator
         {
             while (true)
             {
-                int menuOption = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("4.Exit");
-                );
-                Console.WriteLine("3.mnoznik");
 
+                Console.WriteLine("0.Exit");
                 Console.WriteLine("1.Dodawanie");
-                Console.WriteLine("2.Odejmowanie"
+                Console.WriteLine("2.Odejmowanie");
+                Console.WriteLine("3. mnozenie");
+                Console.WriteLine("4. dzielenie");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
                 if (menuOption == 4)
                 {
                     break;
